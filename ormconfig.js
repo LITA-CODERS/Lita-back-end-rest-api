a2d26509e8bee5f101a4c6c3af1ad89998ec5248
Welisson <welissonluca17@gmail.com>
2021-07-09 00:44:15 -0300

    feat: add environment variables connection db

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  type: '',
-  port: '',
-  host: '',
-  username: '',
-  password: '',
-  database: '',
+  type: process.env.DB_TYPE,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   migrations: ['./src/shared/infra/typeorm/migrations/**.ts'],
   entities: ['./src/modules/*/infra/typeorm/entities/**.ts'],
   cli: {
