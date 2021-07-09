62c0e1dcd91b52e2822993c750c94f5c467101fd
Welisson <welissonluca17@gmail.com>
2021-07-09 09:10:41 -0300

    feat: add entity and repository foodDishe

---
+++ src/modules/foodDishes/infra/typeorm/entities/FoodDishes.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+
+@Entity('food_dishes')
+class FoodDishes {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image_url: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    this.id = this.id ?? uuid();
+  }
+}
+
+export { FoodDishes };
