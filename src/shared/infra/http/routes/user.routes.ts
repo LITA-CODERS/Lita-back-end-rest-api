918cabf4ab9c0ce14a823a2ab9beba906487d4c7
Welisson <welissonluca17@gmail.com>
2021-07-10 17:25:44 -0300

    feat: add route put update user

--- src/shared/infra/http/routes/user.routes.ts
+++ src/shared/infra/http/routes/user.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 
-import { CreateUserController } from '@modules/accounts/useCases/CreateUser/CreateUserController';
+import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
 import { ListUsersController } from '@modules/accounts/useCases/listUsers/ListUsersController';
+import { UpdateUserController } from '@modules/accounts/useCases/updateUser/UpdateUserController';
 
 const UserRouter = Router();
 
 const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
+const updateUserController = new UpdateUserController();
 UserRouter.post('/users', createUserController.handle);
 UserRouter.get('/users', listUsersController.handle);
+UserRouter.put('/users/:id', updateUserController.handle);
 export { UserRouter };
