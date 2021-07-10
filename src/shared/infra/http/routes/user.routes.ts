1249d080732ca7f04278a5245aa124655e330687
Welisson <welissonluca17@gmail.com>
2021-07-10 18:10:10 -0300

    feat: add find by email  user  controller and use case and routes]

--- src/shared/infra/http/routes/user.routes.ts
+++ src/shared/infra/http/routes/user.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { DeleteUserController } from '@modules/accounts/useCases/deleteUser/DeleteUserController';
+import { FindUserByEmailUseController } from '@modules/accounts/useCases/findUserByEmail/FindUserByEmailController';
 import { ListUsersController } from '@modules/accounts/useCases/listUsers/ListUsersController';
 import { UpdateUserController } from '@modules/accounts/useCases/updateUser/UpdateUserController';
 
@@ -9,7 +11,11 @@ const UserRouter = Router();
 const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
 const updateUserController = new UpdateUserController();
+const findUserByEmailUseController = new FindUserByEmailUseController();
+const deleteUserController = new DeleteUserController();
 UserRouter.post('/users', createUserController.handle);
 UserRouter.get('/users', listUsersController.handle);
-UserRouter.put('/users/:id', updateUserController.handle);
+UserRouter.get('/user/email/:email', findUserByEmailUseController.handle);
+UserRouter.put('/user/:id', updateUserController.handle);
+UserRouter.delete('/user/:id', deleteUserController.handle);
 export { UserRouter };
