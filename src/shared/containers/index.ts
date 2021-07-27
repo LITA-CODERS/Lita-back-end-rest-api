2dddeb22fc5aa03d8c47916452530e3ade1fb504
Welisson <welissonluca17@gmail.com>
2021-07-26 21:28:15 -0300

    feat: add singleton food dishes repository

--- src/shared/containers/index.ts
+++ src/shared/containers/index.ts
@@ -2,5 +2,11 @@ import { container } from 'tsyringe';
 
 import { UserRepository } from '@modules/accounts/infra/typeorm/repositories/UserRepository';
 import { IUserRepository } from '@modules/accounts/repositories/IUserRepository';
+import { IFoodDishesRepository } from '@modules/foodDishes/repositories/IFoodDishesRepository';
+import { FoodDishesRepository } from '@modules/foodDishes/infra/typeorm/repositories/FoodDishesRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+container.registerSingleton<IFoodDishesRepository>(
+  'FoodDishesRepository',
+  FoodDishesRepository
+);
