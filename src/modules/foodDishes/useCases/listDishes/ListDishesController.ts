da6bd4b6e81ac2d548b00e5ecabf02af2f6a0aad
Welisson luca <welissonluca17@gmail.com>
2021-08-01 22:06:28 -0300

    feat: add list dishes

---
+++ src/modules/foodDishes/useCases/listDishes/ListDishesController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ListDishesUseCase } from './ListDishesUseCase';
+
+class ListDishesController {
+  async handle(request: Request, response: Response) {
+    const listDishesUseCase = container.resolve(ListDishesUseCase);
+
+    const result = await listDishesUseCase.execute();
+    return response.status(200).json({
+      message: 'update success',
+      result,
+    });
+  }
+}
+
+export { ListDishesController };
