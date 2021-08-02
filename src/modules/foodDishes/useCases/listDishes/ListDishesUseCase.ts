5d89c0dedeb74f69ad84758dbfdf3fd0a96a37d6
Welisson luca <welissonluca17@gmail.com>
2021-08-01 22:06:28 -0300

    feat: add list dishes

---
+++ src/modules/foodDishes/useCases/listDishes/ListDishesUseCase.ts
@@ -0,0 +1,19 @@
+import { inject, injectable } from 'tsyringe';
+
+import { FoodDishes } from '@modules/foodDishes/infra/typeorm/entities/FoodDishes';
+import { IFoodDishesRepository } from '@modules/foodDishes/repositories/IFoodDishesRepository';
+import { AppError } from '@shared/errors/AppError';
+
+@injectable()
+class ListDishesUseCase {
+  constructor(
+    @inject('FoodDishesRepository')
+    private foodDishesRepository: IFoodDishesRepository
+  ) {}
+  async execute(): Promise<FoodDishes[]> {
+    const result = await this.foodDishesRepository.findAll();
+    if (result.length === 0) throw new AppError(`No food dishes found`);
+    return result;
+  }
+}
+export { ListDishesUseCase };
