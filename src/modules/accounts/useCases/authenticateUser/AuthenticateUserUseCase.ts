f830ddebee22cd3b1a96e40cc924e87a6972cab3
Welisson luca <welissonluca17@gmail.com>
2021-08-05 15:58:18 -0300

    feat: add authenticate user controller and use case'

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,49 @@
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUserRepository } from '@modules/accounts/repositories/IUserRepository';
+import { AppError } from '@shared/errors/AppError';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UserRepository')
+    private userRepository: IUserRepository
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) throw new AppError('Email or password incorrect');
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) throw new AppError('Email or password incorrect');
+
+    const token = sign({}, process.env.TOKEN, {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
