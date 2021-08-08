13228d322fb3f613375e97b8545a56e19bc10a7b
Welisson luca <welissonluca17@gmail.com>
2021-08-08 08:25:20 -0300

    feat: add list user controller specs

---
+++ src/modules/accounts/useCases/listUsers/tests/ListUserController.spec.ts
@@ -0,0 +1,59 @@
+import request from 'supertest';
+import { Connection } from 'typeorm';
+
+import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
+import { app } from '@shared/infra/http/app';
+import { connection } from '@shared/infra/typeorm';
+
+let db: Connection;
+let token;
+let responseUser;
+describe('List users controller', () => {
+  beforeAll(async () => {
+    db = await connection();
+    await db.runMigrations();
+    const createUserDTO: ICreateUserDTO = {
+      name: 'test',
+      email: 'email@gmail.com',
+      password: 'password',
+    };
+    responseUser = await request(app)
+      .post('/accounts/users')
+      .send(createUserDTO);
+    token = await request(app).post('/accounts/login').send({
+      email: 'email@gmail.com',
+      password: 'password',
+    });
+  });
+
+  it('should be able to list a users', async () => {
+    await request(app)
+      .get('/accounts/users')
+      .send()
+      .set({
+        Authorization: `Bearer ${token.body.token}`,
+      })
+      .expect(200);
+  });
+  it('should throw error if list users not exists', async () => {
+    await request(app)
+      .delete(`/accounts/user/${responseUser.body.id}`)
+      .send()
+      .set({
+        Authorization: `Bearer ${token.body.token}`,
+      });
+    await request(app)
+      .get('/accounts/users')
+      .send()
+      .set({
+        Authorization: `Bearer ${token.body.token}`,
+      })
+      .expect(400);
+  });
+  afterAll(async () => {
+    await db.query(
+      'drop table food_dishes; drop table categories; drop table users; drop table migrations;'
+    );
+    await db.close();
+  });
+});
