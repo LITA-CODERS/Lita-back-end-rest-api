49cea9ce5106f4dacb20de1919ad4f52ecc5c150
Welisson luca <welissonluca17@gmail.com>
2021-10-01 15:33:39 -0300

    feat: add perfomance sentry js

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,6 +2,9 @@ import 'reflect-metadata';
 import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 
+import { RewriteFrames } from '@sentry/integrations';
+import * as Sentry from '@sentry/node';
+
 import 'express-async-errors';
 
 import { AppError } from '@shared/errors/AppError';
@@ -9,12 +12,31 @@ import '@shared/containers/index';
 
 import { router } from './routes';
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  integrations: [
+    new RewriteFrames({
+      // eslint-disable-next-line no-underscore-dangle
+      root: global.__dirname,
+    }),
+  ],
+});
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.use(router);
+
+app.get('/debug-sentry', function mainHandler(req, res) {
+  throw new Error('My first Sentry error!');
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
