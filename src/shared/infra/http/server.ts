ba6416bcff383f01272384c8449428c5c1c23dca
Welisson luca <welissonluca17@gmail.com>
2021-10-01 15:13:27 -0300

    feat: add perfomance sentry js

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -9,7 +9,7 @@ import { app } from './app';
 connection();
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: 'https://95684c28645446acaffabea99d7273b7@o1022168.ingest.sentry.io/5988273',
   tracesSampleRate: 1.0,
   integrations: [
     new RewriteFrames({
@@ -17,6 +17,26 @@ Sentry.init({
       root: global.__dirname,
     }),
   ],
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      // Capture all 404 and 500 errors
+      if (error.status === 404 || error.status === 500) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
+
+app.get('/debug-sentry', function mainHandler(req, res) {
+  throw new Error('My first Sentry error!');
 });
 
 app.listen(process.env.PORT || 3333, () => console.log('Server is running!'));
