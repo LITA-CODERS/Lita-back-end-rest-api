6d3b2037c650539b28aef0f536ad212902630310
Welisson luca <welissonluca17@gmail.com>
2021-10-01 14:38:52 -0300

    feat: add sentry js

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,22 @@
 import 'dotenv/config';
 
+import * as Sentry from '@sentry/node';
+import { RewriteFrames } from '@sentry/integrations';
+
 import { connection } from '../typeorm';
 import { app } from './app';
 
 connection();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  integrations: [
+    new RewriteFrames({
+      // eslint-disable-next-line no-underscore-dangle
+      root: global.__dirname,
+    }),
+  ],
+});
+
 app.listen(process.env.PORT || 3333, () => console.log('Server is running!'));
